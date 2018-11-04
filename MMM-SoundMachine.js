1a88891f66994206ae09cbb445e9a3f3216c1ca7
Mykle <25211804+mykle1@users.noreply.github.com>
2018-11-03 20:23:35 -0400

    Add files via upload

--- MMM-SoundMachine.js
+++ MMM-SoundMachine.js
@@ -7,54 +7,67 @@
 
 Module.register("MMM-SoundMachine", {
 	// Default module config.
-	defaults: {
-		sounds: 'rain',                     // 1-52
-		maxWidth: "100%",               // Adjusts size of images. Retains aspect ratio.
-//		updateInterval: 5 * 60 * 1000,  // set in config.js
-//    animationSpeed: 3000,
-	},
+			defaults: {
+				  sounds: 'Rain&Waves',
+					statement: "SoundMachine",
+		},
 
-	start: function () {
-		self = this;
-		this.url = '';
+//	start: function () {
+//		self = this;
+//		this.url = '';
 
-	},
+//	},
 
-	getStyles: function () {
-		return ["MMM-SoundMachine.css"]
-	},
+        getStyles: function () {
+		    return ["MMM-SoundMachine.css"]
+	  },
 
-	// Override dom generator.
-	getDom: function () {
+	     // Override dom generator.
+	     getDom: function () {
 
-		    var wrapper = document.createElement("div");
+			    var wrapper = document.createElement("div");
 
-        var sleep = document.createElement("p");
-				sleep.classList.add("small", "bright", "sleep");
-				sleep.innerHTML = "Relaxing sounds playing";
-				wrapper.appendChild(sleep);
+	        var sleep = document.createElement("div");
+					sleep.classList.add("small", "bright", "sleep");
+					sleep.innerHTML = this.config.statement;
+					wrapper.appendChild(sleep);
 
 
-		if (this.config.sounds != "") {
-			var sound = new Audio();
-			sound.src = 'modules/MMM-SoundMachine/sounds/' + this.config.sounds + '.wav';
-			sound.loop = true;
-			sound.play();
-	}
+		    // this works but there is a gap when looping. Not seamless. :(
+				if (this.config.sounds != "") {
+					var sound = new Audio();
+					sound.src = 'modules/MMM-SoundMachine/sounds/' + this.config.sounds + '.mp3';
+					sound.loop = true;
+					sound.play();
+			  }
+
+	       //////// Add buttons to pause and play sounds ///////////////
+
+					var button = document.createElement('button');
+					button.innerHTML = "<img src='modules/MMM-SoundMachine/icons/pause3.png' height='50' width='50'>";
+		//			button.innerHTML = '<button class="button">Pause</button>';
+			    button.className = ('button');
+					button.addEventListener("click", () =>  sound.pause());
+					wrapper.appendChild(button);
+
+					var button2 = document.createElement('button');
+					button2.innerHTML = "<img src='modules/MMM-SoundMachine/icons/play3.png' height='50' width='50'>";
+		//			button2.innerHTML = '<button class="button">Play</button>';
+			    button2.className = ('button');
+					button2.addEventListener("click", () =>  sound.play());
+					wrapper.appendChild(button2);
 
 		return wrapper;
 	},
-
 
 	/////  Add this function to the modules you want to control with voice //////
 
     notificationReceived: function(notification, payload) {
         if (notification === 'HIDE_SOUNDMACHINE') {
-            this.hide();
+            this.hide(500);
         }  else if (notification === 'SHOW_SOUNDMACHINE') {
             this.show(1000);
         }
-
     },
 
 });
--- README.md
+++ README.md
@@ -1,38 +1,43 @@
-# MMM-SoundMachine
+## MMM-SoundMachine
 
-Designed to imitate the soothing sounds of popular sound machines and sleep machines.
+Sleep, relax, concentrate or meditate to your own favorite sounds.
+
+## What you get
+
+It's pretty simple really. It's more about the idea than the complexity of the module
+
+* One default sound of the rain and beach. (My personal favorite)
+* Pause and play buttons. (Requires the ability to click)
+* Customizable config and css
+
+## Caveat
+
+Sound files can be rather large. That is why I only provided one. I worked at getting
+shorter, smaller sound files to loop seamlessly and I asked for help. There is no apparent solution
+to this very common problem. So, please acquire your favorite mp3 sounds or use the one provided.
+
+## Examples
+
+![](images/1.png)
+
 
 ## Installation
 
-`git clone https://github.com/mykle1/MMM-SoundMachine` into the `~/MagicMirror/modules` directory.`
+* `git clone https://github.com/mykle1/MMM-SoundMachine` into the `~/MagicMirror/modules` directory.
+
+* mp3 sound files all go in the MMM-SoundMachine/sounds folder. One is provided.
+
+* No API key needed! No dependencies needed! No kidding!
+
 
 ## Config.js entry and options
-```
+
 {
     module: 'MMM-SoundMachine',
     header: "",
-    position: 'bottom_right',
-       config: {
-       sounds: 'rain',           // See list at bottom of README
-       maxWidth: "300px",     // If you use a header this would help
+    position: 'bottom center',
+    config: {
+        sounds: 'Rain&Waves', // Just the name of the mp3, not the extension
+        statement: "SoundMachine",
     }
 },
-```
-## Default sounds
-
-* driving
-* furnace
-* people
-* people2
-* rain
-* rain2
-* rain3
-* seashore
-* washroom
-
-## Add your own sounds.
-
-* Use .wav files. MP3's do NOT make good seamless loops
-* The beginning and end of the sound file should sound alike
-* Put the sound files in the MagicMirror/modules/MMM-SoundMachine/sounds folder
-* Put only the name (not the extension) in the config
